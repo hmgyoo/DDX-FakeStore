ecf9171737c35e35709961328f593e483a79ae70
gyoo <ernogarydaniel@gmail.com>
2024-02-12 17:15:21 +0800

    secure connection with faketstore api

---
+++ screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products');
+        setApiData(response.data);
+        setLoading(false);
+      } catch(error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <View>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : apiData ? (
+        <View>
+          {apiData.map((product) => (
+            <Text key={product.id}>{product.title}</Text>
+          ))}
+        </View>
+      ) : (
+        <Text>No data available.</Text>
+      )}
+    </View>
+  );
+};
